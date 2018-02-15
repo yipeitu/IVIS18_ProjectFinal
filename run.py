9308148f863085b65d21dfda3d5ba027163f704b
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-02-15 13:58:33 +0100

    server API

--- run.py
+++ run.py
@@ -1,13 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from script_db import DBmongo
 
 app = Flask(__name__)
 mongo = DBmongo()
+all_uri = ['/', '/goal?goal=the goal number', '/save', '/load']
 
-@app.route('/')
+# catch wrong uri
+@app.route('/<path:path>')
+def catch_all(path):
+    return jsonify({"message": 
+    	"this is the wrong uri, you should use " + ', '.join(all_uri)})
+
+@app.route('/', methods=['GET'])
 def index():
-	return jsonify(mongo.query())
+	return jsonify(mongo.queryAll())
 
-@app.route('/hello')
-def hello():
-	return 'hello world'
+@app.route('/goal', methods=['GET'])
+def query_data():
+	print(request.url_rule)
+	result = mongo.query({"Goal":request.args.get("goal")})
+	if result is None:
+		result = {}
+	return jsonify(result)
+
+@app.route('/save', methods=['POST'])
+def save_data():
+	print(request.is_json)
+	content = request.get_json()
+	print(content)
+	return 'save'
+
+@app.route('/load', methods=['GET'])
+def load_data():
+	return 'load'
+
