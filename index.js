1e05cd715f1b676040f3bfc68f4bb04f71a4c8e3
Lynn <lynnl@kth.se>
2018-02-20 14:57:50 +0100

    Web-Template

--- index.js
+++ index.js
@@ -32,3 +32,30 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 });
+
+
+// LYNNS CODE
+
+$(document).ready(function(){
+	// When page loads...:
+	$("div.content div").hide(); // Hide all content
+	$("nav li:first").addClass("current").show(); // Activate first page
+	$("div.content div:first").show(); // Show first page content
+
+	// On Click Event (within list-element!)
+	$("nav div").click(function() {
+		$("nav a").removeClass("current"); // Remove any active class
+		$(this).addClass("current"); // Add "current" class to selected page
+    console.log(this);
+
+		$("div.content div").hide(); // Hide all content
+    $("div.memberContainer").show(); //Show is div in the contact page
+    $("div.lynn").show();
+    // $("div.lynn2").show();
+
+    // Find the href attribute value to identify the active page+content:
+		var activePage = $(this).find("a").attr("href");
+		$(activePage).show(); // Fade in the active page content
+	}); // end click method
+
+}); // end $(document).ready method
