b2d6bf4e5675c2144f83760f72c26e6e45e33eab
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-02-21 16:00:37 +0100

    get actual data for message box

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // LYNNS CODE
 $(document).ready(function(){
   // When page loads...:
-  // $("div.content").hide(); // Hide all content
   $("div.page").hide();
   $("nav li:first").addClass("current").show(); // Activate first page
   $("div.content div:first").show(); // Show first page content
@@ -10,12 +9,8 @@ $(document).ready(function(){
   $("nav div").click(function() {
     $("nav a").removeClass("current"); // Remove any active class
     $(this).addClass("current"); // Add "current" class to selected page
-    console.log(this);
 
     $("div.page").hide(); // Hide all content
-    // $("div").show(); //Show is div in the contact page
-    // $("div.lynn").show();
-    // $("div.lynn2").show();
 
     // Find the href attribute value to identify the active page+content:
     var activePage = $(this).find("a").attr("href");
@@ -26,50 +21,104 @@ $(document).ready(function(){
 
 var jsonList;
 $.getJSON("https://yipeitu.github.io/IVIS18_ProjectFinal/db_data.json", function(json){
-  // for (var i in json) {
     jsonList = json;
-  // }
 });
 
+var targetPos = function(posList){
+  var html3 = "<div>+3 :</div>";
+  var html2 = "<div>+2 :</div>";
+  var html1 = "<div>+1 :</div>";
+  ["3", "2", "1"].forEach(function(index){
+    switch(index){
+      case "3":
+        if(Object.keys(posList).indexOf(index) !== -1){
+          html3 += `<div>${posList[index]+" "+jsonList[posList[index]].Name}</div>`;
+        }
+        break;
+      case "2":
+        if(Object.keys(posList).indexOf(index) !== -1){
+          html2 += `<div>${posList[index]+" "+jsonList[posList[index]].Name}</div>`;
+        }
+        break;
+      case "1":
+        if(Object.keys(posList).indexOf(index) !== -1){
+          html1 += `<div>${posList[index]+" "+jsonList[posList[index]].Name}</div>`;
+        }
+        break;
+    }
+  })
+  return html3+html2+html1;
+}
+
+var targetNeg = function(negList){
+  if(negList.length === 0) return;
+  var html3 = "<div>-1 :</div>";
+  var html2 = "<div>-2 :</div>";
+  var html1 = "<div>-3 :</div>";
+  ["1", "2", "3"].forEach(function(index){
+    switch(index){
+      case "1":
+        if(Object.keys(negList).indexOf(index) !== -1){
+          html3 += `<div>${negList[index]+" "+jsonList[negList[index]].Name}</div>`;
+        }
+        break;
+      case "2":
+        if(Object.keys(negList).indexOf(index) !== -1){
+          html2 += `<div>${negList[index]+" "+jsonList[negList[index]].Name}</div>`;
+        }
+        break;
+      case "3":
+        if(Object.keys(negList).indexOf(index) !== -1){
+          html1 += `<div>${negList[index]+" "+jsonList[negList[index]].Name}</div>`;
+        }
+        break;
+    }
+  })
+  return html3+html2+html1;
+}
+
+var targetNeu = function(neuList){
+  var html = "<div>0: </div>";
+  neuList.forEach(function(target){
+    html += `<div>${target+" "+jsonList[target].Name}</div>`;
+  })
+  return html;
+}
+
 var getTarget = function(id){
-  var posList = [];
-  var negList = [];
+  var posList = {};
+  var negList = {};
   var neuList = [];
   var tragetAffect = jsonList[id].affect
-  console.log(tragetAffect);
+  
   for (var i in tragetAffect) {
     if (tragetAffect[i] === 0){
       neuList.push(i);
-      // console.log("Neutral" + i);
     }
     else if (tragetAffect[i] < 0){
-      negList.push(i);
-      // console.log("Negative" + i);
+      if(Object.keys(negList).indexOf(tragetAffect[i]) === -1){
+        negList[tragetAffect[i]] = [i];
+      } else {
+        negList[tragetAffect[i]].push(i);
+      }
     }
     else if (tragetAffect[i] > 0){
-      posList.push(i);
-      // console.log("Positive" + i);
+      if(Object.keys(posList).indexOf(tragetAffect[i]) === -1){
+        posList[tragetAffect[i]] = [i];
+      } else {
+        posList[tragetAffect[i]].push(i);
+      }
     }
   }
+
   $("#boxDescription")[0].innerHTML = `<div>name:${jsonList[id].Name}</div>
-              <div>description:${posList}</div>
-              <div>row sum:</div>
-              <div>column sum:</div>
+              <div>description:${jsonList[id].Description}</div>
+              <div>row sum:${jsonList[id].Sum}</div>
+              <div>column sum:${jsonList[id].Sum}</div>
               <div> + positive influence</div>
-              <div>+3:</div>
-              <div>+2:</div>
-              <div>+1:</div>
+              ${targetPos(posList)}
               <div> - negative influence</div>
-              <div>-1:</div>
-              <div>-2:</div>
-              <div>-3:</div>
+              ${targetNeg(negList)}
               <div>no obvious influence</div>
-              <div>0:</div>`
-  //HÄMTA TARGET SOM AFFEKTAR ETT ANNAT TARGET POSITIVT/NEGATIVT/NEUTRALT
-  // document.getElementById("name").innerHTML = "<h1>"+jsonList[id].Name+"</h1>" +;
-  // document.getElementById("page1_container").innerHTML = "<h1>POSITIVE</h1>" + "</br>" + "<p>"+ posList + "</p>";
-  // document.getElementById("page1_container").innerHTML = "<h1>NEUTRAL</h1>" + "</br>" + "<p>"+ neuList + "</p>";
-
-  console.log(jsonList[id].Name);
-
+              ${targetNeu(neuList)}`;
 }
