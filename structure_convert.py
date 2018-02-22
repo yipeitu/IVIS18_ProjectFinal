cc33231258aa9e4bc3819b80e4ecde52c61c326f
Thony Price <thonyprice@gmail.com>
2018-02-22 14:20:12 +0100

    Add id to data

--- structure_convert.py
+++ structure_convert.py
@@ -16,17 +16,17 @@ result = []
 with open("db_data.json", "r") as rFile:
 	data = json.load(rFile)
 	for key in dict.keys(data):
-		temp = {"name": data[key]["Name"], "size": 0, "imports": []}
+		temp = {"name": data[key]["Name"], "size": 0, "imports": [], "id": data[key]["AI"]}
 		for target in dict.keys(data[key]["affect"]):
 			if data[key]["affect"][target] > 0:
-				temp["imports"].append(data[target]["Name"])
+				# temp["imports"].append(data[target]["Name"])
 				# --- This constructs the version with values for connections ---
-				# d = {}
-				# d["target"] = data[target]["Name"]
-				# d["value"] = data[key]["affect"][target]
-				# temp["imports"].append(d)
+				d = {}
+				d["target"] = data[target]["Name"]
+				d["value"] = data[key]["affect"][target]
+				temp["imports"].append(d)
 		result.append(temp)
 
 # with open("structure_data2.json", "w") as wFile:
-with open("structure_data.json", "w") as wFile:
+with open("structure_data3.json", "w") as wFile:
 	json.dump(result, wFile)
