6577b6bf70a44735bc207f926afe2690700fe6be
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-02-27 17:54:17 +0100

    1.files and folers organized 2.Influenced target title/text should be RED when negative influence and GREEN in opposite. 3.Targets not active should be hidden

---
+++ js/visualization.js
@@ -0,0 +1,212 @@
+var diameter = window.innerWidth*0.5,
+    radius = diameter / 2,
+    innerRadius = radius - 180;
+
+var cluster = d3.cluster()
+    .size([360, innerRadius]);
+
+var line = d3.radialLine()
+    .curve(d3.curveBundle.beta(0.5))
+    .radius(function(d) { return d.y; })
+    .angle(function(d) { return d.x / 180 * Math.PI; });
+
+var svg = d3.select(".container-viz").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .append("g")
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+
+var link = svg.append("g").selectAll(".link"),
+    node = svg.append("g").selectAll(".node");
+
+d3.json("https://yipeitu.github.io/IVIS18_ProjectFinal/data/structure_data5.json", function(error, classes) {
+  if (error) throw error;
+  
+  var root = packageHierarchy(classes)
+      .sum(function(d) { return d.size; });
+
+  cluster(root);
+
+  link = link
+    .data(packageImports(root.leaves()))
+    .enter().append("path")
+      .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
+      .attr("class", "link")
+      .attr("style", function(d) {
+        return getStyle(d).concat("stroke-opacity: 0.2;")
+      })
+      .attr("d", line);
+
+  node = node
+    .data(root.leaves())
+    .enter().append("text")
+      .attr("class", "node")
+      .attr("dy", "0.31em")
+      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
+      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+      .text(function(d) { return d.data.key; })
+      .on("mouseover", mouseovered)
+      .on("mouseout", mouseouted)
+      .on("click", getData);
+});
+
+sticky_links = false;
+
+function turnOffStickyLinks() {
+  sticky_links = false;
+  document.getElementById("boxDescription").innerHTML = " ";
+  trigger(mouseouted);
+}
+
+function getData(d) {
+  if (!sticky_links) {
+    getTarget(d.data.id);
+    sticky_links = !sticky_links;
+  }
+}
+
+function getStyle(d) {
+  var t_name = d.target.data.name;
+  var value;
+  d.source.data.imports.forEach(function(d) {
+    if (d.target == t_name) {
+      value = d.value;
+    }
+  });
+  if (value == 3) {
+   color = "#62BF77"; z = 1;
+  } else if (value == 2) {
+   color = "#96CE7E"; z = 2;
+  } else if (value == 1) {
+   color = "#D4E578"; z = 3;
+  } else if (value == 0) {
+   color = "blue"; z = 0;
+  } else if (value == -1) {
+   color = "#F1A772"; z = 3;
+  } else if (value == -2) {
+   color = "#F0686A"; z = 2;
+  } else if (value == -3) {
+   color = "#A54A47";
+   z = 1;
+  }
+  var str1 = "stroke-width: ";
+  var str2 = 2.5**Math.abs(value);
+  var tmp_str = "stroke: ";
+  var z_index = "z-index: ".concat(z.toString(), ";");
+  var position = "position: absolute;";
+  if (value != 0) {
+    return str1.concat(str2.toString(), ";", tmp_str, color, ";", z_index);
+  } else {
+    return "display: none;";
+  }
+}
+
+function mouseovered(d) {
+  if (!sticky_links) {
+    node
+        .each(function(n) { n.target = n.source = false; });
+
+    // Non-relevant links color change
+    link._groups[0].forEach(function(d) {
+         d.style.stroke = "#f4f4f4";
+         //d.style.opacity = 0.2;
+         });
+
+    link
+      .classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
+      .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+      .filter(function(l) { return l.source === d; })
+      .attr("style", function(d) {
+        return getStyle(d);
+      })
+      .raise();
+
+    // node
+    //     .classed("node--target", function(n) { return n.target; })
+    //     .classed("node--source", function(n) { return n.source; });
+    // class
+    // node: neutral, grey
+    // node-source: positive, green
+    // node-target: negative, red
+    // var neuNodeList = []
+    var posSourceList = []
+    var negTargetList = []
+    d.data.imports.forEach(function(target){
+      if(target.value > 0){
+        posSourceList.push(target.target);
+      }
+      else if(target.value < 0){
+        negTargetList.push(target.target);
+      }
+      // else{
+      //   neuNodeList.push(target.target);
+      // }
+    })
+    node
+        .classed("node--target", function(n) {
+          return negTargetList.indexOf(n.data.name) != -1
+        }) // negative
+        .classed("node--source", function(n) {
+          return posSourceList.indexOf(n.data.name) != -1
+        }) // positive
+  }
+}
+
+function mouseouted(d) {
+  if (!sticky_links) {
+    link
+        .classed("link--target", false)
+        .classed("link--source", false)
+        .attr("style", function(d) {
+          return getStyle(d).concat("stroke-opacity: 0.2;");
+        })
+
+    node
+        .classed("node--target", false)
+        .classed("node--source", false);
+  }
+}
+
+// Lazily construct the package hierarchy from class names.
+function packageHierarchy(classes) {
+  var map = {};
+
+  function find(name, data) {
+    var node = map[name], i;
+    if (!node) {
+      node = map[name] = data || {name: name, children: []};
+      if (name.length) {
+        node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+        node.parent.children.push(node);
+        node.key = name.substring(i + 1);
+      }
+    }
+    return node;
+  }
+
+  classes.forEach(function(d) {
+    find(d.name, d);
+  });
+
+  return d3.hierarchy(map[""]);
+}
+
+// Return a list of imports for the given array of nodes.
+function packageImports(nodes) {
+  var map = {},
+      imports = [];
+
+  // Compute a map from name to node.
+  nodes.forEach(function(d) {
+    map[d.data.name] = d;
+  });
+
+  // For each import, construct a link from the source to target node.
+  nodes.forEach(function(d) {
+    if (d.data.imports) d.data.imports.forEach(function(i) {
+      imports.push(map[d.data.name].path(map[i.target]));
+    });
+  });
+
+  return imports;
+}
