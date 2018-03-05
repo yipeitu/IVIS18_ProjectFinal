078581b652f3ef1438bf81d34934f943f78c1779
Thony Price <thonyprice@gmail.com>
2018-03-05 23:15:10 +0100

    Download func for json files

--- js/download.js
+++ js/download.js
@@ -1,3 +1,13 @@
-function download() {
-  console.log("Starting download. . . ");
+function download_json(filename, text) {
+  var text = decodeURI(text); // Test should be received encoded
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename + ".json");
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
