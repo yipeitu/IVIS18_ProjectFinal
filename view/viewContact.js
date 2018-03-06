b8f415e9b269b2a97ade0ba19a49f64541793925
Lucas Grönborg <lucas94gronborg@gmail.com>
2018-03-06 10:58:19 +0100

    Styled the contact page

--- view/viewContact.js
+++ view/viewContact.js
@@ -1,3 +1,3 @@
-$("#page4")[0].innerHTML = `<h1>This is the contact page</h1>
+$("#page4")[0].innerHTML = `<h1>The team</h1>
   <!-- Team Member Template -->
   <div class="memberContainer" id="memberContainer">`;
